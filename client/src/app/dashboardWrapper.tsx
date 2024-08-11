8f49765e147eb4094e53054c55025d5fca35e274
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-10 20:54:57 -0400

    update dashboardWrapper

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,17 +1,51 @@
+"use client";
+
+import { useEffect } from "react";
+
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+// ! Specific way of using Redux with NextJS
+// Having <StoreProvider> wraps around specific component and add 'use client' on top
+
+// If we, instead, add <StoreProvider> in main layout.tsx and add 'use client' there,
+// we will get an error
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  });
+
   return (
-    <div className={`light flex min-h-screen w-full bg-gray-50 text-gray-900`}>
+    <div
+      className={`${isDarkMode ? "dark" : "light"} flex min-h-screen w-full bg-gray-50 text-gray-900`}
+    >
       <Sidebar />
       <main
-        className={`flex h-full w-full flex-col bg-gray-50 px-9 py-7 md:pl-24`}
+        className={`flex h-full w-full flex-col bg-gray-50 px-9 py-7 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 export default DashboardWrapper;
