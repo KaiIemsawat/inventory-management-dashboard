db86cdf6971b7cb86d6547f8a9a5ee8a0628c25a
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-18 19:16:12 -0400

    applied mui > dataGrid

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -1,7 +1,34 @@
 "use client";
 
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
 import { useGetProductsQuery } from "@/state/api";
 import Header from "@/app/(components)/Header";
+
+const columns: GridColDef[] = [
+  { field: "productId", headerName: "ID", width: 90 },
+  { field: "name", headerName: "Product Name", width: 200 },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => `$${row.price}`,
+  },
+  {
+    field: "rating",
+    headerName: "Rating",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => (row.rating ? row.rating : "N/A"),
+  },
+  {
+    field: "stockQuantity",
+    headerName: "Stock Quantity",
+    width: 150,
+    type: "number",
+  },
+];
 
 const Inventory = () => {
   const { data: products, isError, isLoading } = useGetProductsQuery();
@@ -20,6 +47,13 @@ const Inventory = () => {
   return (
     <div className="flex flex-col">
       <Header name="Inventory" />
+      <DataGrid
+        rows={products}
+        columns={columns}
+        getRowId={(row) => row.productId}
+        checkboxSelection
+        className="mt-5 rounded-lg border border-gray-200 bg-white !text-gray-700 shadow"
+      />
     </div>
   );
 };
